6dc5693f74b478cb85f218a81ecb3d93ff0deb22
danivehl <144905638+danivehl@users.noreply.github.com>
2023-09-29 11:11:59 +0000

    Fri, Sep 29, 2023, 11:11 AM +00:00

--- chatbot.c
+++ chatbot.c
@@ -2,4 +2,74 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-int d(const char*a,const char*b){int la=strlen(a),lb=strlen(b),i,j,D[99]={la};for(i=1;i<=la;i++)D[i]=i;for(i=1;i<=lb;i++)for(j=la,D[0]=i;j;j--)D[j]=a[--j]==b[i-1]?D[j-1]:1+(D[j]<D[j-1]?D[j]:D[j-1]);return D[la];}int main(){char u[99];srand(time(0));printf("Stupid Chatbot: Hello! What can I do for you today?\n");while(1){printf("You: ");fgets(u,99,stdin);u[strlen(u)-1]='\0';const char*r[]={"Hi there!","Hello!","Greetings!","Hey!","I'm just a computer program, so I don't have feelings.","I'm here to assist you, so I'm doing well!","I'm as good as the code that powers me!","I don't have a name. You can call me Chatbot!","I go by Chatbot. How can I help you today?","I'm just Chatbot, here to assist you!"};int m=99,i=sizeof(r)/sizeof(r[0]);while(i--){int x=d(u,r[i]);if(x<m)m=x,printf("Stupid Chatbot: %s\n",r[i]);}if(m>5){const char*x[]={"Sorry, I don't understand. Can you please rephrase?","I'm not sure what you mean. Could you try again?","Hmmm, that's a bit confusing. Can you provide more details?","I'm just a simple chatbot, and I didn't get that. Can you reword your question?"};printf("Stupid Chatbot: %s\n",x[rand()%4]);}}return 0;}
+
+// Function to calculate how similar two words are
+int calculateSimilarity(const char *word1, const char *word2) {
+    int length1 = strlen(word1);
+    int length2 = strlen(word2);
+    int matrix[99] = {length1};
+
+    for (int i = 1; i <= length1; i++) {
+        matrix[i] = i;
+    }
+
+    for (int i = 1; i <= length2; i++) {
+        for (int j = length1, matrix[0] = i; j; j--) {
+            matrix[j] = (word1[j - 1] == word2[i - 1]) ? matrix[j - 1] : 1 + ((matrix[j] < matrix[j - 1]) ? matrix[j] : matrix[j - 1]);
+        }
+        matrix[length1] = matrix[length1 - 1];
+    }
+
+    return matrix[length1];
+}
+
+int main() {
+    char userInput[100];
+    srand(time(NULL));
+
+    printf("Chatbot: Hi! How can I help you today?\n");
+
+    while (1) {
+        printf("You: ");
+        fgets(userInput, sizeof(userInput), stdin);
+        userInput[strlen(userInput) - 1] = '\0';
+
+        const char *responses[] = {
+            "Hi there!", "Hello!", "Greetings!", "Hey!",
+            "I'm just a computer program, so I don't have feelings.",
+            "I'm here to assist you, so I'm doing well!",
+            "I'm as good as the code that powers me!",
+            "I don't have a name. You can call me Chatbot!",
+            "I go by Chatbot. How can I help you today?",
+            "I'm just Chatbot, here to assist you!"
+        };
+
+        int minSimilarity = 99;
+        int bestResponseIndex = -1;
+
+        for (int i = 0; i < sizeof(responses) / sizeof(responses[0]); i++) {
+            int similarity = calculateSimilarity(userInput, responses[i]);
+            if (similarity < minSimilarity) {
+                minSimilarity = similarity;
+                bestResponseIndex = i;
+            }
+        }
+
+        if (bestResponseIndex != -1) {
+            printf("Chatbot: %s\n", responses[bestResponseIndex]);
+        } else if (minSimilarity > 5) {
+            const char *unknownResponses[] = {
+                "I'm not sure what you mean. Can you try again?",
+                "Can you rephrase that, please?",
+                "I didn't quite catch that. Could you provide more details?",
+                "I'm a simple chatbot, and that's a bit confusing. Can you ask differently?"
+            };
+            int randomIndex = rand() % (sizeof(unknownResponses) / sizeof(unknownResponses[0]));
+            printf("Chatbot: %s\n", unknownResponses[randomIndex]);
+        } else {
+            printf("Chatbot: Sorry, I don't know how to respond to that.\n");
+        }
+    }
+
+    return 0;
+}
